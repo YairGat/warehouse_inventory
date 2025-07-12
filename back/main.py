8cfc8946e3f8cd9eebd6165dd3354141e6c79528
Illay Sasson <illaysasson5@gmail.com>
2025-07-12 15:59:01 +0300

    added elad changes

--- back/main.py
+++ back/main.py
@@ -321,4 +321,4 @@ if __name__ == '__main__':
     if not os.path.exists(db_path):
         with app.app_context():
             db.create_all()
-    app.run(debug=True, port=5001, host="0.0.0.0")
+    app.run(debug=False, port=5001, host="0.0.0.0", ssl_context=("cert.pem", "priv.pem"))
